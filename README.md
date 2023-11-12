5b45a0e44bb1badfc5a5c95126f160299f857eb3
Ghulam Murtaza <murtazarb99@gmail.com>
2023-11-12 20:03:02 +0500

    feat: deploy

---
+++ README.md
@@ -0,0 +1,31 @@
+IssueCraft - Modern Full-Stack Issue Tracker
+License
+PRs Welcome
+
+Overview
+IssueCraft is a state-of-the-art issue tracker designed for modern development teams. This project leverages the power of Next.js for the app router, Tailwind CSS for a sleek UI, Radix UI for seamless UI components, Prisma for database interactions, and MySQL for data storage.
+
+Features
+Intuitive UI: A clean and responsive user interface for effortless navigation.
+Powerful Back-end: Next.js powers the app router, Prisma handles database interactions, and MySQL ensures data integrity.
+Customizable Components: Radix UI provides a library of flexible and customizable UI components.
+Collaborative Issue Tracking: Streamline project management with a robust issue tracking system.
+Getting Started
+To get started with IssueCraft, follow these simple steps:
+
+Clone the repository: git clone https://github.com/yourusername/issuecraft.git
+Install dependencies: npm install
+Configure your database connection in the .env file.
+Run migrations: npx prisma migrate dev
+Start the development server: npm run dev
+For detailed instructions and customization options, refer to our Documentation.
+
+Contributing
+We welcome contributions! Please follow our Contribution Guidelines to contribute to IssueCraft.
+
+License
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+Happy Coding! 🚀
+
+Feel free to reach out with any questions or feedback. We're excited to see how IssueCraft can enhance your development workflow.
---
+++ app/issues/new/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import React, { useState } from "react";
+import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
+
+const NewIssuePage = () => {
+  const router = useRouter();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitting(false);
+      setError("An unexpected error occurred.");
+    }
+  });
+
+  return (
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="space-y-3" onSubmit={onSubmit}>
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button disabled={isSubmitting} className="hover:cursor-pointer">
+          Submit New Issue
+          {isSubmitting && <Spinner />}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewIssuePage;
